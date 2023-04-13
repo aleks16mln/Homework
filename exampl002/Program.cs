84623f619471f64b6b9c5bdff7c74e52d321a730
Aleksey Ivanov <16mln@mail.ru>
2023-04-13 18:56:08 +0300

    задача4

---
+++ exampl002/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите в столбик три числа:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
